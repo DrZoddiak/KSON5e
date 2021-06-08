caa20690de5c9aebeafc8c9130de10882b15413d
Jesse McKee <DrZoddiak@gmail.com>
2021-06-08 08:01:26 -0500

    Remove un-necessary imports

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
     java
     maven
-    kotlin("jvm") version "1.4.21"
+   // kotlin("jvm") version "1.4.21"
 }
 
 group "com.github.DrZoddiak"
-version "1.0-SNAPSHOT"
-
-group "dev.divinegenesis"
 version "1.0-SNAPSHOT"
 
 repositories {
@@ -20,11 +17,4 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:3.8.0")
-    implementation(kotlin("stdlib"))
-    testImplementation(kotlin("test"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
