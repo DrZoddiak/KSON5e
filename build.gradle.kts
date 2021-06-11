b80ef989230feed047f5d221a738dd618ef2c4df
Jesse McKee <jessemckee97@gmail.com>
2021-06-11 03:15:18 -0500

    I think I finally got it!

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,40 +13,44 @@ project.version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
 dependencies {
-    api("com.google.code.gson:gson:2.8.7") {
-        isTransitive = true
-    }
-    api("com.squareup.retrofit2:retrofit:2.9.0") {
-        isTransitive = true
-    }
-    api("com.squareup.retrofit2:converter-gson:2.9.0") {
-        isTransitive = true
-    }
-    compileOnly("com.squareup.okhttp3:logging-interceptor:3.8.0")
-    compileOnly(kotlin("stdlib-jdk8"))
+    api("com.google.code.gson:gson:2.8.7")
+    api("com.squareup.retrofit2:retrofit:2.9.0")
+    api("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:3.8.0")
+    shadow(kotlin("stdlib-jdk8"))
 }
 
 val shadowJar: ShadowJar by tasks
+val jar: Jar by tasks
 val build: Task by tasks
+
 
 shadowJar.archiveClassifier.set("FatJar")
 
+jar.manifest {
+    attributes(mapOf("Main-Class" to "dev.divinegenesis.JTK"))
+}
+jar.enabled = false
+
+build.dependsOn(shadowJar)
+
 tasks.withType<ShadowJar> {
-    archiveBaseName.set("Kson5e")
-    exclude("*.pom")
-    relocate("com.squareup.retrofit","dev.divinegenesis.retrofit")
     mergeServiceFiles()
-    manifest {
-        attributes(mapOf("Main-Class" to "dev.divinegenesis.JTK"))
-    }
+    archiveBaseName.set("Kson5e")
+    archiveClassifier.set("")
+    archiveVersion.set("1.0")
+    relocate("retrofit2","dev.divinegenesis.retrofit2")
+    relocate("com.google","dev.divinegenesis.google")
 }
 
-build.apply {
-    dependsOn(shadowJar)
+
+
+tasks.withType<AbstractArchiveTask> {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
 }
 
 tasks {
